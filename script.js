c7c5634ee366662ae1b7b9e3f639997cc6b03259
Natasha Chiorsac <75411738+natashachiorsac@users.noreply.github.com>
2022-08-14 19:33:26 +1000

    implemented buttons to change colour modes

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 let gridSize = 16;
 let drawColour = '#3A3A3A';
-let isColorMode = false;
-const maxSize = 64;
+let isColourMode = false;
+const colours = ["#d9ce9e", "#68c2d3",
+     "#b8354f", "#3f4873",
+    "#a1cc95", "#3c7368"]
 
+/* Generate the grid */
 function generateGrid(gridSize){
     const container = document.querySelector('#container');
     container.setAttribute('style', `grid-template-columns: repeat(${gridSize}, 1fr)`);
@@ -17,6 +20,9 @@ function generateGrid(gridSize){
         newSquare.classList.add("square");
         newSquare.addEventListener('mouseover', () => {
             newSquare.style.background = drawColour;
+            if (isColourMode){
+                drawColour = colours[Math.floor(Math.random()*colours.length)];
+            }
         });
         container.appendChild(newSquare);
     }
@@ -24,21 +30,30 @@ function generateGrid(gridSize){
 
 generateGrid(gridSize);
 
-const reset = document.getElementById("reset");
+/* Reset button */
+const resetButton = document.getElementById("reset");
 
-reset.addEventListener('click', () => {
+resetButton.addEventListener('click', () => {
     generateGrid(gridSize);
 });
 
+/* Change colour mode */
+const classicModeButton = document.getElementById("classic-mode");
+const colourModeButton = document.getElementById("colour-mode");
 
-function setModeToColour(){
-    return 0;
-}
+// Change to regular mode
+classicModeButton.addEventListener('click', () => {
+    isColourMode = false;
+    drawColour = '#3A3A3A';
+});
 
-function setModeToRegular(){
-    return 0;
-}
+// Change to colour mode
+colourModeButton.addEventListener('click', () => {
+    isColourMode = true;
+    drawColour = colours[Math.floor(Math.random()*colours.length)];
+});
 
+/* Change grid size */
 const slider = document.getElementById("range-slider");
 
 slider.oninput = function(){
